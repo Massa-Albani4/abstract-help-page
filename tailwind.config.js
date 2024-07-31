653679c23d39133ba45e4c39c571db997a0bcffc
Massa-Albani4 <massa.albani0213@gmail.com>
2024-07-31 14:04:41 +0300

    Add design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        100: "0.375rem",
+        200: "1rem",
+        300: "1.25rem",
+        400: "1.875rem",
+        500: "3rem",
+      },
+    },
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      gray: "#A4A5A6",
+      "blue-100": "#DADBF1",
+      "blue-500": "#4C5FD5",
+    },
+    fontFamily: {
+      "GT-America": ['"GT-America"', "sans-serif"],
+    },
   },
   plugins: [require("tailwindcss-debug-screens")],
 };
