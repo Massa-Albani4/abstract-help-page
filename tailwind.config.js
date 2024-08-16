2f2cf70d3533b45417d74c3560e208615c277590
Massa-Albani4 <massa.albani0213@gmail.com>
2024-08-16 22:58:33 +0300

    Add features lg styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       },
     },
     colors: {
-      black: "#000000",
+      black: "#191A1B",
+      "black-200": "#000000",
       white: "#FFFFFF",
       "blue-100": "#DADBF1",
       "blue-200": "#4C5FD5",
