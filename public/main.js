70c501cf31d4bf59b27225fa7a3f6a3740ca77c0
Massa Albani <massa.albani0213@gmail.com>
2025-11-26 22:37:08 +0400

    Update hero section styles for tablets

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const menuBtn = document.getElementById("menu-btn");
 const navMenu = document.getElementById("nav-menu");
 const navbar = document.getElementById("navbar");
 const menuOpen = document.getElementById("menu-open");
-const menuExit = document.getElementById("menu-exit");
+const menuClose = document.getElementById("menu-close");
 
 function toggleMenu() {
   if (navMenu.classList.contains("hidden")) {
@@ -11,13 +11,13 @@ function toggleMenu() {
     navbar.classList.add("bg-black");
     navbar.classList.remove("bg-black-200");
     menuOpen.classList.add("hidden");
-    menuExit.classList.remove("hidden");
+    menuClose.classList.remove("hidden");
   } else {
     navMenu.classList.add("hidden");
     navMenu.classList.remove("grid");
     navbar.classList.remove("bg-black");
     navbar.classList.add("bg-black-200");
     menuOpen.classList.remove("hidden");
-    menuExit.classList.add("hidden");
+    menuClose.classList.add("hidden");
   }
 }
